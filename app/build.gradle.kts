bcee3abf67997fe4d0c618b097c7a94cf34bc5e8
camilo <camilo.swe@gmail.com>
2025-11-10 19:10:47 -0500

    chore: Bump version to 0.2.1 for release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "com.carryzonemap.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 3
-        versionName = "0.2.0"
+        versionCode = 4
+        versionName = "0.2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
