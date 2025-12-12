457eeb0340cc135cfa783fd9ec8e16aac2f9a78e
camiloh12 <camiloh1286@gmail.com>
2025-12-12 17:07:50 -0500

    release version and notes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "com.carryzonemap.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 4
-        versionName = "0.2.1"
+        versionCode = 5
+        versionName = "0.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
